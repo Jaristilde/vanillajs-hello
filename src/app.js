c865775e75942968a71459f0702d35a331021d7b
Jaristilde <154548762+Jaristilde@users.noreply.github.com>
2024-01-04 01:58:01 +0000

    javascript project completed for prework

--- src/app.js
+++ src/app.js
@@ -2,10 +2,45 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateExcuse() {
+  let pronoun = ["A", "The"];
+  let subject = ["teacher", "goat", "cat", "driver", "mechanic"];
+  let action = ["took my", "ate my", "tore my", "drove my", "broke my"];
+  let object = ["homework", "banana", "clothe", "car", "ratchet"];
+  let location = [
+    "in the classroom",
+    "in my farm",
+    "in my house",
+    "in my garage",
+    "in my workshop"
+  ];
+
+  let proindx = Math.floor(Math.random() * pronoun.length);
+  let subindx = Math.floor(Math.random() * subject.length);
+  let actindx = Math.floor(Math.random() * action.length);
+  let objindx = Math.floor(Math.random() * object.length);
+  let locindx = Math.floor(Math.random() * location.length);
+
+  let excuse =
+    pronoun[proindx] +
+    " " +
+    subject[subindx] +
+    " " +
+    action[actindx] +
+    " " +
+    object[objindx] +
+    " " +
+    location[locindx];
+
+  return excuse;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const paragraph = document.querySelector("#the-excuse");
+  paragraph.textContent = generateExcuse();
+
+  const button = document.getElementById("btn");
+  button.addEventListener("click", function() {
+    paragraph.textContent = generateExcuse();
+  });
 };
